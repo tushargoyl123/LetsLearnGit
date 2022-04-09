90d3d29cac1f61414dab26a8367c9aad32ccb0fb
tushargoyl123 <tushargoyl123@gmail.com>
2022-04-09 21:58:17 +0530

    added message in sum

--- Sum.java
+++ Sum.java
@@ -6,6 +6,6 @@ public class Sum {
     public static void main(String[] args) {
         int a = 5;
         int b = 6;
-        System.out.println(a+b);
+        System.out.println("the sum is given as" + (a+b));
     }
 }
